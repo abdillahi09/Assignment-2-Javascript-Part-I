187aa40c51bf9a4148d4a9a5430bb9f9664730dd
Abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-07-10 16:27:42 -0700

    Add files via upload

---
+++ assignmenttwo.js
@@ -0,0 +1,36 @@
+// Assignment Two
+// Create following variables and assign give values
+var recipeName = "Creamy-squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Details");
+console.log("--------------")
+console.log("Recipe Name:", recipeName);
+console.log("Preparation Time:", preparationTime);
+console.log("Recipe By:", recipeBy);
+console.log("is Vegetarian Dish:",isVegetarian);
+console.log("Calories:", calories);
+console.log("Recipe details after update");
+
+// Contains updated variables
+
+var preparationTime = 60;
+var lastName = "George";
+console.log("--------------")
+console.log("Recipe Name:", recipeName);
+console.log("Preparation Time:", preparationTime);
+console.log("Recipe By:", recipeBy + " " + lastName);
+console.log("is Vegetarian Dish:",isVegetarian);
+console.log("Calories:", calories);
+
+
+
+
+
+
+
+
+
